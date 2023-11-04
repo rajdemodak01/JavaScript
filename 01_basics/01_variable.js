a6181795ca7f494b757ffb14f049703d9b3a863f
Raj De Modak <rajgobindadham@gmail.com>
2023-11-04 19:12:37 +0000

    dataTypes updated

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="rajgobindadham@gmail.com"
+var accountPassword="12345"
+accountCity="Kolkata"
+
+// accountId=2
+//this is not allowed
+console.log(accountId);
+
+accountEmail="rakk@gmail.com"
+accountPassword="2154"
+accountCity="Bankura"//this method to declare variable is not preferred
+let accountState;
+/*
+    Prefer not to use var because of the issue in block  scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) //We are using nodeJS, not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+console.log(3+3);
+console.log("Raj");
+
+let name="raj" //string datatype
+let age=21//number datatype
+let isloggedIn=true//bool datatype
+let state=null
+
+console.table([name,age,isloggedIn,state]);
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=>standalobe value
+//undefined=>when value is not defined but the variable is declared
+//symbol=>used to find uniqueness
+
+
+//object
+
+console.log(typeof "raj")
+console.log(typeof 5)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
