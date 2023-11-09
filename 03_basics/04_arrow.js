33566d9dadf05a4f12dea060e9e8e40b744427d8
Raj De Modak <rajgobindadham@gmail.com>
2023-11-09 08:31:49 +0000

    arrow updated

--- 03_basics/04_arrow.js
+++ 03_basics/04_arrow.js
@@ -6,9 +6,45 @@ const user={
         console.log(this);
     }
 }
-user.welcomeMessage()
+// user.welcomeMessage()
 user.username="Shyam"
-user.welcomeMessage()
+// user.welcomeMessage()
 // console.log(user.username);
 
-console.log(this);//{}
+// console.log(this);//{}
+
+function func(){
+    let username="Raj De Modak"
+    console.log(this.username);
+}
+// func()//undefined
+
+// const result=function(){
+//     let username="Raj De Modak"
+//     console.log(this.username);
+// }
+// result()//undefined
+
+const result=()=>{//arrow function
+    let username="Raj De Modak"
+    console.log(this.username);//undefined
+    console.log(this);//{}
+}
+
+// result()
+// ()=>{}//arrow function
+
+const addTwo=(num1,num2)=>{
+    return num1+num2//explicit return
+}
+// console.log(addTwo(2,4));
+const addThree=(num1,num2,num3)=> num1+num2+num3///implicit return(we do not use return for one statement)
+
+// console.log(addThree(2,4,6));
+
+const objectReturn=(num1,num2)=>({username:"Raj"})
+console.log(objectReturn(1,2));//{ username: 'Raj' }
+
+const myArray=[1,2,5,3,5]
+// myArray.forEach(()=>{return })
+// myArray.forEach(()=>())
